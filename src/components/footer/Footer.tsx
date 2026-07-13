d7726606e76152e44bfdf48f28e13bab429cc198
talitatech <76888906+talitatech@users.noreply.github.com>
2026-07-13 10:00:35 -0300

    Cria componentes iniciais

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="w-full bg-blue-600 text-white text-center py-4">
+      Farmácia Popular | Generation Brasil
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Farmácia Popular</h1>
+    </div>
+  )
+}
+
+export default Home
